c343fcea0482e9b736921e6bf61e60340892159c
Isaac Meza <i.mezal.12@gmail.com>
2024-06-21 20:18:34 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,9 @@ project = "Nested Nonparametric Instrumental Variable Regression"
 copyright = '2024, Meza, I. and Singh, R.'
 author = "Isaac Meza and Rahul Singh"
 
+
 # -- General configuration ---------------------------------------------------
+# -- General configuration
 
 extensions = [
     "sphinx.ext.duration",
@@ -25,10 +27,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
-    "sphinx.ext.napoleon",  # Add napoleon for Google/NumPy style docstrings
 ]
-
-autosummary_generate = True  # Automatically generate summary files
 
 intersphinx_mapping = {
     "rtd": ("https://docs.readthedocs.io/en/stable/", None),
@@ -42,10 +41,19 @@ templates_path = ["_templates"]
 # -- Options for EPUB output
 epub_show_urls = "footnote"
 
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
 html_theme = "sphinx_rtd_theme"
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
