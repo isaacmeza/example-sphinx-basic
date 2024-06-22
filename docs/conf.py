04c8b7800a5adb7120fc0b0e689eda94c2bb5ce5
Isaac Meza <i.mezal.12@gmail.com>
2024-06-21 19:32:14 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))  # Adjust the path to include the project root
+sys.path.insert(0, os.path.abspath('../../semiparametrics'))  # Include the semiparametrics directory
 
 
 # -- Project information -----------------------------------------------------
