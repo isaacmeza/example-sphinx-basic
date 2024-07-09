5edabebf03b1ce2ef6d1af34dadeb99dfbde0f09
Isaac Meza <i.mezal.12@gmail.com>
2024-07-09 03:01:51 -0600

    Update sparse_l1_l1.py

--- mliv/linear/sparse_l1_l1.py
+++ mliv/linear/sparse_l1_l1.py
@@ -13,7 +13,7 @@ Classes:
 import numpy as np
 from sklearn.linear_model import Lasso, LassoCV, ElasticNet
 from sklearn.base import clone
-from .utilities import cross_product
+from mliv.linear.utilities import cross_product
 
 
 class _SparseLinearAdversarialGMM:
