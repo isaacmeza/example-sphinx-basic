b586362d89af363c94332a3e4eb6c9e97d595cf5
Isaac Meza <i.mezal.12@gmail.com>
2024-07-10 21:17:51 -0600

    Update sparse2_l2_l2.py

--- mliv/linear/sparse2_l2_l2.py
+++ mliv/linear/sparse2_l2_l2.py
@@ -119,11 +119,7 @@ class sparse2_l2vsl2(_SparseLinear2AdversarialGMM):
         """
         Calculate the duality gap to certify convergence of the algorithm.
 
-        The ensembles $\bar{\alpha}$ and $\bar{\theta_1}$ can be thought of as primal and dual solutions, respectively. The duality gap is used as a certificate for convergence of the algorithm.
-
-        \begin{align*}
-        \text{Duality Gap} &:= \max_{\|\theta_1\|_1 \leq 1 } L(\bar{\alpha}, \theta_1) - \min_{\|\alpha\|_1 \leq V_1} L(\alpha, \bar{\theta_1})
-        \end{align*}
+        The ensembles can be thought of as primal and dual solutions, and the duality gap can be used as a certificate for convergence of the algorithm.
 
         Parameters:
             A (array-like): Covariates for the first stage.
@@ -354,11 +350,7 @@ class sparse2_ridge_l2vsl2(_SparseLinear2AdversarialGMM):
         """
         Calculate the duality gap to certify convergence of the algorithm.
 
-        The ensembles $\bar{\alpha}$ and $\bar{\theta_1}$ can be thought of as primal and dual solutions, respectively. The duality gap is used as a certificate for convergence of the algorithm.
-
-        \begin{align*}
-        \text{Duality Gap} &:= \max_{\|\theta_1\|_1 \leq 1 } L(\bar{\alpha}, \theta_1) - \min_{\|\alpha\|_1 \leq V_1} L(\alpha, \bar{\theta_1})
-        \end{align*}
+        The ensembles can be thought of as primal and dual solutions, and the duality gap can be used as a certificate for convergence of the algorithm.
 
         Parameters:
             A (array-like): Covariates for the first stage.
